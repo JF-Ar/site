8147228051df993086b6f377d26be3baba1a9df8
Josué J Araujo <josue.fa2010@gmail.com>
2022-01-15 10:35:34 -0300

    pagina de cadastro

---
+++ autenticacao/views.py
@@ -0,0 +1,36 @@
+from django.http import response, HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/auth/cadastro')
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome cadastrado')
+            return redirect('/auth/cadastro')
+
+        try:
+            user = User(username= username, email= email, password= senha)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+            return redirect('/auth/logar')
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema, tente novamente mais tarde')
+            return redirect('/auth/cadastro')
+
+
+def logar(request):
+    return HttpResponse('logar')
